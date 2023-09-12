cca28d26299a991a784365244302852fb97f5ca2
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 21:57:22 +0600

    Add project files.

---
+++ Mod_2_11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CompareAreas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+
+            Console.WriteLine("Введите радиус круга:");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону квадрата:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            double S_kruga = pi * r * r;
+            double S_kvad = a * a;
+
+            if (S_kruga > S_kvad)
+            {
+                Console.WriteLine("Площадь круга больше.");
+            }
+            else if (S_kruga < S_kvad)
+            {
+                Console.WriteLine("Площадь квадрата больше.");
+            }
+            else
+            {
+                Console.WriteLine("Площади равны.");
+            }
+        }
+    }
+}
